4456da8d4236d0a51eade3a261ae8f5f2a4da333
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-17 12:23:49 +0700

    upload: index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import {AppRegistry} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+import App from './src/App';
+import {name as appName} from './app.json';
+import {PlaybackService} from './src/services/PlaybackService';
+
+AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => PlaybackService);
